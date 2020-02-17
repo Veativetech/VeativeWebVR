95943c0278d65f0f219cadffa51911c5f0bcc02b
Justin W. Flory <git@jwf.io>
2020-02-17 12:25:02 -0500

    DO NOT MERGE: Test Jenkins CI integration by deleting code
    This commit deletes all code and assets under the `Structure_of_Phenol`
    game. This is done to test what happens when an external contributor
    opens a pull request on the Veativetech/VeativeWebVR repo. The expected
    outcome is a failed test since this commit deletes critical code that is
    used in the unit tests.
    
    The failed test results should be reported in the pull request.
    
    Signed-off-by: Justin W. Flory <git@jwf.io>

--- Structure_of_Phenol/appdata.js
+++
@@ -1,8 +0,0 @@
-app_module_id = "SS200049"
-app_module_name = "Structure of phenol";
-app_module_version = "1.0";
-app_device_platform = "";
-
-// Login profile data
-app_unicef_user_id = "";
-app_server_ip="";
